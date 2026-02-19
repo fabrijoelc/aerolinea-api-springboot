6210b85d72a704756db9755178190733d2a8b0ae
fabrijoelc <fabrizioallcca590@gmail.com>
2026-02-18 22:36:04 -0500

    actualizado pasajerocontroller

--- src/main/java/com/codigo/spring/controller/PasajeroController.java
+++ src/main/java/com/codigo/spring/controller/PasajeroController.java
@@ -22,6 +22,6 @@ public class PasajeroController {
     @PostMapping("/save")
     public PasajeroEntity save(@RequestBody PasajeroEntity pasajeroEntity){
         PasajeroEntity savedEntity = pasajeroRepository.save(pasajeroEntity);
-        return pasajeroRepository.save(savedEntity);
+        return savedEntity;
     }
 }
