af6623e50d08fcbd9f6c0e7f942dce9876aa1500
fabrijoelc <fabrizioallcca590@gmail.com>
2026-02-21 18:03:32 -0500

    Agregado AvionController y AvionRepository

--- src/main/java/com/codigo/spring/controller/AvionController.java
+++ src/main/java/com/codigo/spring/controller/AvionController.java
@@ -1,11 +1,12 @@
 package com.codigo.spring.controller;
 
+import com.codigo.spring.entity.AerolineaEntity;
 import com.codigo.spring.entity.AvionEntity;
 import com.codigo.spring.repository.AvionRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/avion")
@@ -13,7 +14,7 @@ public class AvionController {
 
     private AvionRepository avionRepository;
 
-    public AvionController(AvionRepository avionRepository){
+    public AvionController(AvionRepository avionRepository) {
         this.avionRepository = avionRepository;
     }
 
@@ -21,4 +22,27 @@ public class AvionController {
     public AvionEntity save(@RequestBody AvionEntity avionEntity) {
         return avionRepository.save(avionEntity);
     }
+
+    @GetMapping("/find/{id}")
+    public AvionEntity findById(@PathVariable Long id) {
+        Optional<AvionEntity> optionalAvion = avionRepository.findById(id);
+
+        if (optionalAvion.isPresent()) {
+            return optionalAvion.get();
+        }
+        return null;
+    }
+
+    @GetMapping("/find/modelo")
+    public List<AvionEntity> findAll(@RequestParam String modelo) {
+        List<AvionEntity> avionEntities1 = avionRepository.findByModelo(modelo);
+        List<AvionEntity> avionEntities2 = avionRepository.customFindByModelo(modelo);
+        return avionEntities1;
+    }
+
+    @GetMapping("/find/capacidad")
+    public List<AvionEntity> findAllCapacidad(@RequestParam int min, @RequestParam int max){
+        return avionRepository.findByCapacidad(min, max);
+    }
+
 }
