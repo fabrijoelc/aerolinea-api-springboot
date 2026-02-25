34611c8c46774a16c8200eb96256550442c80786
fabrijoelc <fabrizioallcca590@gmail.com>
2026-02-25 14:18:27 -0500

    Actualizado PilotoEntity

--- src/main/java/com/codigo/spring/entity/PilotoEntity.java
+++ src/main/java/com/codigo/spring/entity/PilotoEntity.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class PilotoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_piloto")
     private int id;
     private String nombre;
     private String apellido;
@@ -26,7 +27,7 @@ public class PilotoEntity {
     @ManyToMany
     @JoinTable(name = "vuelo_piloto",
     joinColumns = @JoinColumn(name = "id_piloto_fk"),
-    inverseJoinColumns = @JoinColumn(name = "id_vuelofk"))
+    inverseJoinColumns = @JoinColumn(name = "id_vuelo_fk"))
     private List<VueloEntity> vuelos;
 
 
