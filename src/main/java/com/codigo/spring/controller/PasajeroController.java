38f83232d6234a619052182dd30f012b7c0e7643
fabrijoelc <fabrizioallcca590@gmail.com>
2026-04-14 11:24:24 -0500

    Actualizado

--- src/main/java/com/codigo/spring/controller/PasajeroController.java
+++ src/main/java/com/codigo/spring/controller/PasajeroController.java
@@ -9,6 +9,11 @@ import com.codigo.spring.repository.PasajeroRepository;
 import com.codigo.spring.response.PasajeroInfoResponse;
 import com.codigo.spring.response.ResponseBase;
 import com.codigo.spring.utils.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -43,17 +48,38 @@ public class PasajeroController {
         return null;
     }
 
+    @Operation(summary = "Trae la informacion del pasajero, incluyendo su boleto y vuelo")
+    @ApiResponse(responseCode = "404", description = "XXXXX",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PasajeroInfoResponse.class),
+                    examples = @ExampleObject(value = "{\n" +
+                            "   \"code\": 404,\n" +
+                            "   \"message\": \"Datos encontrados correctamente.\",\n" +
+                            "   \"data\": {\n" +
+                            "       \"nombre\": \"Scottie\",\n" +
+                            "       \"apellido\": \"Tellett\",\n" +
+                            "       \"boletos\": null\n" +
+                            "   }\n" +
+                            "}")
+            ))
     @GetMapping("/find/boleto/{id}")
     public ResponseBase<PasajeroInfoResponse> findBoleto(@PathVariable int id) {
         List<BoletosEntity> boletos = boletoRepository.findBoletosbyPasajeroId(id);
+        PasajeroEntity pasajero = pasajeroRepository.findById(id).orElse(null);
 
-        if(boletos.isEmpty()){
+        if(pasajero == null){
             return null;
         }
 
         PasajeroInfoResponse pasajeroInfoResponse = new PasajeroInfoResponse();
-        pasajeroInfoResponse.setNombre(boletos.get(0).getPasajero().getNombre());
-        pasajeroInfoResponse.setApellido(boletos.get(0).getPasajero().getApellido());
+        pasajeroInfoResponse.setNombre(pasajero.getNombre());
+        pasajeroInfoResponse.setApellido(pasajero.getApellido());
+
+        if(boletos.isEmpty()){
+            return new ResponseBase<>(Constants.CODE_NOT_FOUND, Constants.MESSAGE_NOT_FOUND, Optional.of(pasajeroInfoResponse));
+        }
+
         pasajeroInfoResponse.setBoletos(new ArrayList<>());
 
         for (BoletosEntity boleto : boletos) {
